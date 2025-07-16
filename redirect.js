cc5faed509af2c17cfaa4f448c094017cd1a0a5f
dytsou <d.y.tsou@gmail.com>
2025-07-16 13:52:52 +0800

    Fix: Update redirect URLs to point to personal site

--- redirect.js
+++ redirect.js
@@ -1,13 +1,17 @@
-(function() {
+(function () {
   var path = window.location.pathname;
   if (path.startsWith("/")) {
     path = path.substring(1);
   }
-  var targetUrl = "https://people.cs.nycu.edu.tw/~dytsou/" + path;
-  
+  var targetUrl = "https://tsou.me" + path;
+
+  if (path === "") {
+    targetUrl = "https://tsou.me/about";
+  }
+
   window.location.replace(targetUrl);
-  
-  document.addEventListener("DOMContentLoaded", function() {
+
+  document.addEventListener("DOMContentLoaded", function () {
     var link = document.getElementById("redirect-link");
     if (link) {
       link.href = targetUrl;
